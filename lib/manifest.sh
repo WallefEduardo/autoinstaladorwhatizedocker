7b023e248be4525d278dbd088cb3dfeb7b0bc16b
root <root@v2202506254070358296.ultrasrv.de>
2026-01-16 00:57:22 +0100

    Atualiza

--- lib/manifest.sh
+++ lib/manifest.sh
@@ -3,11 +3,11 @@
 # Manifesto - carrega todas as bibliotecas
 #
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LIB_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source "${SCRIPT_DIR}/colors.sh"
-source "${SCRIPT_DIR}/utils.sh"
-source "${SCRIPT_DIR}/validation.sh"
-source "${SCRIPT_DIR}/docker.sh"
-source "${SCRIPT_DIR}/ssh.sh"
-source "${SCRIPT_DIR}/menu.sh"
+source "${LIB_DIR}/colors.sh"
+source "${LIB_DIR}/utils.sh"
+source "${LIB_DIR}/validation.sh"
+source "${LIB_DIR}/docker.sh"
+source "${LIB_DIR}/ssh.sh"
+source "${LIB_DIR}/menu.sh"
